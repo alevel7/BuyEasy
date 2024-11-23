031c5f9d899ea57faaf0474639b32345cfc07135
Taiwo kazeem <kazeem@kobo360.com>
2024-11-23 15:00:27 +0100

    set up api gateway

--- gateway/index.js
+++ gateway/index.js
@@ -7,8 +7,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use("/customer", proxy("http://localhost:3000"))
+app.use("/shopping", proxy("http://localhost:3002"))
+app.use("/", proxy("http://localhost:3001")) //product
 
-
-app.listen(3000, () => {
-    console.log("Server started on port 3000");
+app.listen(8000, () => {
+    console.log("Gateway Server started on port 3000");
 });
